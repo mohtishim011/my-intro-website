709d43c799a49d314e9b982df4158637a3c03fa3
mohtishim011 <mohtishimshabir9@gmail.com>
2026-08-15 15:27:28 +0530

    Add login function for user authentication

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function changeText() {
-    document.getElementById("message").innerText =
-        "Great! Tumhari JavaScript kaam kar rahi hai 🎉";
+function login() {
+
+    const username =
+        document.getElementById("username").value.trim();
+
+    const password =
+        document.getElementById("password").value;
+
+    const error =
+        document.getElementById("error");
+
+
+    // Demo login
+    if (username === "mohtishim" && password === "1234") {
+
+        document.getElementById("loginPage").style.display = "none";
+
+        document.getElementById("portfolio").style.display = "block";
+
+        window.scrollTo(0, 0);
+
+    } else {
+
+        error.innerText =
+            "Username ya password galat hai!";
+    }
 }
